c9b2297a895198c5e1b11e0fd38219afc17b84e9
Sambya AS <sambya@aryasa.net>
2015-01-20 23:26:51 +0800

    [Sam]Hello World

---
+++ cocktail-assassin/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  cocktail-assassin
+//
+//  Created by Sambya Aryasa on 20/1/15.
+//  Copyright (c) 2015 tw. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.whiteColor()
+        NSLog("Hello World!")
+    }
+}
