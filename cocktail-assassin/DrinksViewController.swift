980cc0ba67ebc6df953139b1171f0bf073da010b
Sambya AS <sambya@aryasa.net>
2015-05-12 22:11:59 +0800

    add production recipes!

--- cocktail-assassin/DrinksViewController.swift
+++ cocktail-assassin/DrinksViewController.swift
@@ -31,16 +31,17 @@ class DrinksViewController: UIViewController, iCarouselDataSource, iCarouselDele
     var items:[Drink] = []
 
     func initIngredients(){
-        Ingredient.newIngredient("Dark Rum", pumpNumber: 1, amountLeft: 100, managedContext: managedContext!)
+        Ingredient.newIngredient("Light Rum", pumpNumber: 1, amountLeft: 100, managedContext: managedContext!)
         Ingredient.newIngredient("Vodka", pumpNumber: 2, amountLeft: 100, managedContext: managedContext!)
         Ingredient.newIngredient("Gin", pumpNumber: 3, amountLeft: 100, managedContext: managedContext!)
-        Ingredient.newIngredient("White Rum", pumpNumber: 4, amountLeft: 50, managedContext: managedContext!)
-        Ingredient.newIngredient("Lime Juice", pumpNumber: 5, amountLeft: 50, managedContext: managedContext!)
+        Ingredient.newIngredient("Tequila", pumpNumber: 4, amountLeft: 50, managedContext: managedContext!)
+        Ingredient.newIngredient("Triple Sec", pumpNumber: 5, amountLeft: 50, managedContext: managedContext!)
       
-        Ingredient.newIngredient("Coke", pumpNumber: 6, amountLeft: 400, managedContext: managedContext!)
+        Ingredient.newIngredient("Coca Cola", pumpNumber: 6, amountLeft: 400, managedContext: managedContext!)
         Ingredient.newIngredient("Lemonade", pumpNumber: 7, amountLeft: 500, managedContext: managedContext!)
-        Ingredient.newIngredient("Tonic", pumpNumber: 8, amountLeft: 400, managedContext: managedContext!)
-        Ingredient.newIngredient("Cranberry", pumpNumber: 9, amountLeft: 500, managedContext: managedContext!)
+        Ingredient.newIngredient("Orange Juice", pumpNumber: 8, amountLeft: 400, managedContext: managedContext!)
+        Ingredient.newIngredient("Cranberry Juice", pumpNumber: 9, amountLeft: 500, managedContext: managedContext!)
+        Ingredient.newIngredient("Lime Juice", pumpNumber: 10, amountLeft: 500, managedContext: managedContext!)
     }
     
     func createDrinkWithIngredient(name:String, imageName:String, ingredients: Dictionary<String, NSNumber>) -> Drink{
@@ -54,12 +55,62 @@ class DrinksViewController: UIViewController, iCarouselDataSource, iCarouselDele
     
     func initDatabase() {
         initIngredients()
-        items.append(createDrinkWithIngredient("Rum and Coke", imageName: "cocktail-1", ingredients:["Dark Rum":30, "Coke":50]))
-        items.append(createDrinkWithIngredient("Vodka and Lime", imageName:"cocktail-2", ingredients:["Vodka":30, "Lime Juice":50]))
-        items.append(createDrinkWithIngredient("Gin and Cranberry", imageName:"cocktail-3", ingredients:["Gin":30, "Cranberry":50]))
-        items.append(createDrinkWithIngredient("Metropolitan", imageName:"cocktail-4", ingredients:["Vodka":30, "Lime Juice":50, "Cranberry":45]))
-        items.append(createDrinkWithIngredient("Mixin", imageName:"cocktail-5", ingredients:["Dark Rum":30, "Gin":50, "Vodka":20, "Lime Juice":10, "Cranberry":45]))
-        items.append(createDrinkWithIngredient("Long Island Ice Tea", imageName:"cocktail-5", ingredients:["White Rum":30, "Gin":30, "Vodka":30, "Lime Juice":10, "Coke":200]))
+        items.append(createDrinkWithIngredient("Dirty Texas Tea",
+            imageName: "dirty-texas-tea",
+            ingredients: ["Vodka": 23, "Light Rum": 23, "Tequila": 23, "Triple Sec": 23, "Orange Juice": 45, "Coca Cola": 150]))
+        
+        items.append(createDrinkWithIngredient("Alpine Lemonade",
+            imageName: "alpine-lemonade",
+            ingredients: ["Vodka": 30, "Gin": 30, "Light Rum": 30, "Lemonade": 60, "Cranberry Juice": 60]))
+        
+        items.append(createDrinkWithIngredient("The Ollie",
+            imageName: "the-ollie",
+            ingredients: ["Vodka": 60, "Light Rum": 30, "Tequila": 30, "Lemonade": 150]))
+        
+        items.append(createDrinkWithIngredient("Cosmopolitan Classic",
+            imageName: "cosmopolitan-classic",
+            ingredients: ["Vodka": 23, "Triple Sec": 15, "Cranberry Juice": 30, "Lime Juice": 15]))
+        
+        items.append(createDrinkWithIngredient("Margarita",
+            imageName: "margarita",
+            ingredients: ["Tequila": 30, "Triple Sec": 30, "Lime Juice": 15]))
+        
+        items.append(createDrinkWithIngredient("Vodka Cranberry",
+            imageName: "vodka-cranberry",
+            ingredients: ["Vodka": 30, "Cranberry Juice": 135, "Lime Juice": 15, "Orange Juice": 45]))
+        
+        items.append(createDrinkWithIngredient("Black Widow",
+            imageName: "black-widow",
+            ingredients: ["Vodka": 30, "Cranberry Juice": 30, "Lemonade": 30]))
+        
+        items.append(createDrinkWithIngredient("Rum and Coke",
+            imageName: "rum-and-coke",
+            ingredients: ["Light Rum": 30, "Coca Cola": 150]))
+        
+        items.append(createDrinkWithIngredient("Mix Your Own!!",
+            imageName: "mix-your-own",
+            ingredients: ["Light Rum": 0, "Vodka": 0, "Gin": 0, "Tequila": 0, "Triple Sec": 0, "Coca Cola": 0, "Lemonade": 0, "Orange Juice": 0, "Cranberry Juice": 0]))
+        
+        items.append(createDrinkWithIngredient("Hula-Hula",
+            imageName: "hula-hula",
+            ingredients: ["Gin": 60, "Orange Juice": 30, "Triple Sec": 5 ]))
+        
+        items.append(createDrinkWithIngredient("Kamikaze",
+            imageName: "kamikaze",
+            ingredients: ["Vodka": 15, "Triple Sec": 8]))
+        
+        items.append(createDrinkWithIngredient("Gimlet",
+            imageName: "gimlet",
+            ingredients: ["Gin": 90, "Lime Juice": 30]))
+        
+        items.append(createDrinkWithIngredient("Screwdriver",
+            imageName: "screwdriver",
+            ingredients: ["Vodka": 45, "Orange Juice": 120]))
+        
+        
+        items.append(createDrinkWithIngredient("Vodka Lemonade",
+            imageName: "vodka-lemonade",
+            ingredients: ["Vodka": 30, "Lemonade": 150]))
     }
     
     override func viewWillAppear(animated: Bool) {
